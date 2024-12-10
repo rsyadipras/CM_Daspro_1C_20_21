a5ecc5974d7e12173541eda866355b7fc12c70d6
rahmanlana <rahmanlana044@gmail.com>
2024-12-10 13:01:09 +0700

    commit_kedua

--- src/manajemen_Inventory/Index.java
+++ src/manajemen_Inventory/Index.java
@@ -2,6 +2,7 @@ package manajemen_Inventory;
 
 public class Index {
     public static void main(String[] args) {
+        System.out.println("Perkenalkan nama saya nor rahman maulana");
 
     }
 }
