31ed083f2ae87bbfa1b890c51c1f83d68304563f
rahmanlana <rahmanlana044@gmail.com>
2024-12-10 13:06:52 +0700

    commit_ke3

--- src/manajemen_Inventory/PratikumStudiKasus.java
+++ src/manajemen_Inventory/PratikumStudiKasus.java
@@ -3,13 +3,14 @@ package manajemen_Inventory;
 public class PratikumStudiKasus {
     public static void main(String[] args) {
 
-        System.out.println("===== Menu Inventori Cafe =====");
-        System.out.println("1. Tampilkan Menu Inventori");
-        System.out.println("2. Tambah Stok Untuk Item yang Ada");
-        System.out.println("3. Tambah Item Baru");
-        System.out.println("4. Keluar");
-
         while (true) {
+            System.out.println("===== Menu Inventori Cafe =====");
+            System.out.println("1. Tampilkan Menu Inventori");
+            System.out.println("2. Tambah Stok Untuk Item yang Ada");
+            System.out.println("3. Tambah Item Baru");
+            System.out.println("4. Keluar");
+
+            System.out.print("Pilih menu: ");
             System.out.print("Pilih menu: ");
         }
     }
